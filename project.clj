feb9d7b6dde658bea58b54a9ebbb6b01d217850c
Yogthos <dmitri.sotnikov@gmail.com>
2012-10-17 23:28:58 -0400

    first commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject itext-min 
+  "0.1-SNAPSHOT"
+  :description "iText, a free Java-PDF library"
+  :url "https://github.com/yogthos/clj-pdf"
+  :license {:name "LGPL 3.0"
+            :url "http://www.gnu.org/licenses/gpl-3.0.html"
+            :distribution :repo
+            :comments "this is a fork of iText 2.1.7 used by clj-pdf"}  
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :java-source-path "src/main/java" ;lein 1.7
+  ;:java-source-paths ["src/main/java"]
+  )
