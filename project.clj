faf22b2b55773f02d60c377597e683bc5daa520d
Yogthos <dmitri.sotnikov@gmail.com>
2012-10-18 00:18:25 -0400

    updated dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :license {:name "LGPL 3.0"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"
             :distribution :repo
-            :comments "this is a fork of iText 2.1.7 used by clj-pdf"}  
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+            :comments "this is a fork of iText 2.1.7 used by clj-pdf"}    
   :java-source-path "src/main/java" ;lein 1.7
-  ;:java-source-paths ["src/main/java"]
+  ;:java-source-paths ["src/main/java"] ;lein 2
   )
